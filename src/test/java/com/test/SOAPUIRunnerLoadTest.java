575c9e82d1f47e78c85b19ecc849837318182029
runapal5 <runapal5@gmail.com>
2017-03-09 15:53:31 +0530

    Update SOAPUIRunnerLoadTest.java

--- src/test/java/com/test/SOAPUIRunnerLoadTest.java
+++ src/test/java/com/test/SOAPUIRunnerLoadTest.java
@@ -125,6 +125,182 @@ public class SOAPUIRunnerLoadTest {
 
 	   } 
 	   
+	   
+	   @Test
+        public void test1() {
+		
+		  try { 
+                    String projectFile = "/var/lib/jenkins/workspace/App_Exec_LoadTest_TestNG/Pizzas-soapui-project.xml";
+					WsdlProject project = new WsdlProject(projectFile); 
+					for (TestSuite testSuite : project.getTestSuiteList()) { 
+
+					System.out.println("TestSuiteProject size......" + project.getTestSuiteList().size()); 
+					System.out.println("TestSuiteProject......" + testSuite.getName()); 
+
+							for (TestCase testCase : testSuite.getTestCaseList()) { 
+
+									System.out.println("TestCases........." + testCase.getName()); 
+									
+
+									
+
+									StringToObjectMap properties = new StringToObjectMap(); 
+									//properties.put("username", userName); 
+									//properties.put("password", passWord); 
+									//properties.put("localhost", localHost); 
+									//properties.put("HttpDefaultPort", port); 
+									List<TestStep> testSteps = testCase.getTestStepList(); 
+
+
+									// TestRunner runner = testCase.run(properties, false); 
+									WsdlTestCaseRunner wsdlTestCaseRunner = new WsdlTestCaseRunner((WsdlTestCase) testCase, properties); 
+									WsdlTestRunContext wsdlTestRunContext = wsdlTestCaseRunner.createContext(properties); 
+									wsdlTestCaseRunner.run(); 
+
+
+                                    
+									WsdlTestCase wsdlTestCase = wsdlTestCaseRunner.getTestRunnable(); 
+									System.out.println("Test case " + wsdlTestCase); 
+									
+
+                                    
+									List<LoadTest> loadtest = wsdlTestCase.getLoadTestList(); 
+									System.out.println("Load test cases " + loadtest); 
+									Iterator<LoadTest> loadIterator = loadtest.iterator(); 
+									System.out.println("Load test size...." + loadtest.size()); 
+
+									while (loadIterator.hasNext()) { 
+											LoadTest loadTest = loadIterator.next(); 
+
+											System.out.println("Load Test " + loadTest.getName()); 
+											LoadTestRunner runner = loadTest.run(); 
+											
+											// wait for test to finish 
+											while (!runner.hasStopped()) { 
+												if (runner.getStatus() == Status.RUNNING) { 
+													System.out.println("LoadTest [" + loadTest.getName() + "] progress: " + runner.getProgress() + ", " 
+															+ runner.getRunningThreadCount()); 
+												} 
+												Thread.sleep(1000); 
+											} 
+								 
+											System.out.println("LoadTest [" + loadTest.getName() + "] finished with status " + runner.getStatus().toString()); 
+                                            
+ 
+											//loadTest.getStatisticsModel().finish(); 
+							                 
+											//Assert.assertEquals( LoadTestRunner.Status.FAILED , runner.getStatus()); 
+											Assert.assertEquals( LoadTestRunner.Status.FAILED , runner.getStatus()); 
+											
+											System.out.println("Exporting log and statistics for LoadTest [" + loadTest.getName() + "]"); 
+											 
+											
+											 
+									} //end of while of loadRunner
+                                    
+									
+							} 
+
+					    //System.exit(1); 
+					} 
+
+			  } catch (Exception e) { 
+			         e.printStackTrace(); 
+			  } 
+
+
+
+
+	   } 
+	   
+	   
+	   @Test
+       public void test2() {
+		
+		  try { 
+                    String projectFile = "/var/lib/jenkins/workspace/App_Exec_LoadTest_TestNG/Pizzas-soapui-project.xml";
+					WsdlProject project = new WsdlProject(projectFile); 
+					for (TestSuite testSuite : project.getTestSuiteList()) { 
+
+					System.out.println("TestSuiteProject size......" + project.getTestSuiteList().size()); 
+					System.out.println("TestSuiteProject......" + testSuite.getName()); 
+
+							for (TestCase testCase : testSuite.getTestCaseList()) { 
+
+									System.out.println("TestCases........." + testCase.getName()); 
+									
+
+									
+
+									StringToObjectMap properties = new StringToObjectMap(); 
+									//properties.put("username", userName); 
+									//properties.put("password", passWord); 
+									//properties.put("localhost", localHost); 
+									//properties.put("HttpDefaultPort", port); 
+									List<TestStep> testSteps = testCase.getTestStepList(); 
+
+
+									// TestRunner runner = testCase.run(properties, false); 
+									WsdlTestCaseRunner wsdlTestCaseRunner = new WsdlTestCaseRunner((WsdlTestCase) testCase, properties); 
+									WsdlTestRunContext wsdlTestRunContext = wsdlTestCaseRunner.createContext(properties); 
+									wsdlTestCaseRunner.run(); 
+
+
+                                    
+									WsdlTestCase wsdlTestCase = wsdlTestCaseRunner.getTestRunnable(); 
+									System.out.println("Test case " + wsdlTestCase); 
+									
+
+                                    
+									List<LoadTest> loadtest = wsdlTestCase.getLoadTestList(); 
+									System.out.println("Load test cases " + loadtest); 
+									Iterator<LoadTest> loadIterator = loadtest.iterator(); 
+									System.out.println("Load test size...." + loadtest.size()); 
+
+									while (loadIterator.hasNext()) { 
+											LoadTest loadTest = loadIterator.next(); 
+
+											System.out.println("Load Test " + loadTest.getName()); 
+											LoadTestRunner runner = loadTest.run(); 
+											
+											// wait for test to finish 
+											while (!runner.hasStopped()) { 
+												if (runner.getStatus() == Status.RUNNING) { 
+													System.out.println("LoadTest [" + loadTest.getName() + "] progress: " + runner.getProgress() + ", " 
+															+ runner.getRunningThreadCount()); 
+												} 
+												Thread.sleep(1000); 
+											} 
+								 
+											System.out.println("LoadTest [" + loadTest.getName() + "] finished with status " + runner.getStatus().toString()); 
+                                            
+ 
+											//loadTest.getStatisticsModel().finish(); 
+							                 
+											//Assert.assertEquals( LoadTestRunner.Status.FAILED , runner.getStatus()); 
+											Assert.assertEquals( LoadTestRunner.Status.FAILED , runner.getStatus()); 
+											
+											System.out.println("Exporting log and statistics for LoadTest [" + loadTest.getName() + "]"); 
+											 
+											
+											 
+									} //end of while of loadRunner
+                                    
+									
+							} 
+
+					    //System.exit(1); 
+					} 
+
+			  } catch (Exception e) { 
+			         e.printStackTrace(); 
+			  } 
+
+
+
+
+	   } 
+	   
 	      
 
 							
